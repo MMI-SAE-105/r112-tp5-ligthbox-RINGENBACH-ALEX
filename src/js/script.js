bd662f83f00d1decb0bdb0defcb0eda84ffb953c
RINGENBACH-ALEX <alex.ringenbach@edu.univ-fcomte.fr>
2024-12-03 14:28:19 +0100

    rédaction du JS vu ce matin en cours

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,24 @@
 // *** LIGHTBOX ***
+const lightbox = document.querySelector("#lightbox");
+const lightboxImg = document.querySelector("#lightbox > img");
+
+// Pour déléguer l'événement click sur les images
+document.body.addEventListener("click", (evt) => {
+    if (evt.target.matches("[data-full-img]")) {
+        lightboxImg.src = evt.target.dataset.fullImg;
+        lightbox.showModal();
+    }
+});
+
+lightbox.addEventListener("click", (evt) => {
+    lightbox.classList.add("sortie");
+    lightbox.addEventListener(
+        "animationend",
+        () => {
+            const lightbox = null;
+            lightbox.classList.remove("sortie");
+            lightbox.close();
+        },
+        { once: true }
+    );
+});
